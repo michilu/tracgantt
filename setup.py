1b5ad8cac51422c2920b24bdd79e0ff728a4748e
Takanao Endoh <djmchl@gmail.com>
2009-04-29 23:30:53 +0900

    Tagged TracGantt 0.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup (
         name = "TracGantt",
-        version = "0.3.2a",
+        version = "0.3.3",
         packages = ['tracgantt'],
         package_data={'tracgantt': ['templates/*.cs', 'htdocs/*.css']},
 
